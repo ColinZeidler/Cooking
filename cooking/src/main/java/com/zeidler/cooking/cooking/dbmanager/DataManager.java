87f46baff0015f0200f0f7202fffe5e115aaef8b
Colin Zeidler <c.zeidler.wc@gmail.com>
2014-04-18 02:01:49 -0400

    bumped db version to clear problem caused by a crash

--- cooking/src/main/java/com/zeidler/cooking/cooking/dbmanager/DataManager.java
+++ cooking/src/main/java/com/zeidler/cooking/cooking/dbmanager/DataManager.java
@@ -19,7 +19,7 @@ import java.util.Random;
  */
 public class DataManager extends SQLiteOpenHelper{
 
-    public static final int    DATABASE_VERSION = 5;
+    public static final int    DATABASE_VERSION = 6;
     public static final String  DATABASE_NAME   = "cookbook";
 
     private static final String S_TABLENAME     = "steps";
