ded30a657754008f44c59635344a4843b656364f
Colin Zeidler <c.zeidler.wc@gmail.com>
2014-04-30 11:16:27 -0400

    step navigation is partially working,
    able to navigate through the positions for the steps, but no data is displayed
    when at the end of the list navigation breaks down

--- cooking/src/main/java/com/zeidler/cooking/cooking/recipedetails/StepDetailActivity.java
+++ cooking/src/main/java/com/zeidler/cooking/cooking/recipedetails/StepDetailActivity.java
@@ -3,6 +3,7 @@ package com.zeidler.cooking.cooking.recipedetails;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -16,6 +17,10 @@ import java.util.List;
 
 /**
  * Created by Colin on 2014-04-30.
+ * Activity that presents the user the details of a specific step
+ *
+ * Navigation to next / prev steps is possible via the nav buttons in the action bar,
+ * or swiping the screen from side to side todo implement this
  */
 public class StepDetailActivity extends Activity {
     private Context mContext;
@@ -36,7 +41,7 @@ public class StepDetailActivity extends Activity {
         ActionBar ab = getActionBar();
 
         if (ab != null) {
-            ab.setTitle("Test");
+            ab.setTitle("Step " + (mPos+1));
             ab.setDisplayHomeAsUpEnabled(true);
         }
     }
@@ -50,11 +55,27 @@ public class StepDetailActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
+        Intent stepI = new Intent();
+        stepI.putExtra("steps", (ArrayList<Step>) mSteps);
+        stepI.setClass(mContext, StepDetailActivity.class);
+        //TODO step navigation is a little sketchy at the ends
         switch (item.getItemId()) {
             case R.id.action_next:
+                if (mPos == mSteps.size()-1) {
+                    finish();
+                } else {
+                    stepI.putExtra("pos", ++mPos);
+                    startActivity(stepI);
+                }
                 return true;
             case R.id.action_prev:
-                finish();
+                if (mPos == 0) {
+                    finish();
+                } else {
+                    stepI.putExtra("pos", --mPos);
+                    startActivity(stepI);
+                }
                 return true;
         }
         return super.onOptionsItemSelected(item);
